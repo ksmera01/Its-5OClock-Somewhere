34a5eeab95d3653b4a25eedef15863592368c274
Kevin Smeraglio <ksmera01@gmail.com>
2020-04-10 22:25:05 -0400

    updated js and results code

--- assets/script.js
+++ assets/script.js
@@ -204,33 +204,9 @@ $().ready(function () {
                 for (var i = 0; i < response.drinks.length; i++) {
 
                     var drinkArray = response.drinks[i];
+                    var ingredArray = [];
 
                     console.log(drinkArray);
-
-                    var ingredientsAndMeasure1 = ((drinkArray.strIngredient1 + " - " + drinkArray.strMeasure1));
-                    console.log(ingredientsAndMeasure1);
-                    var ingredientsAndMeasure2 = ((drinkArray.strIngredient2 + " - " + drinkArray.strMeasure2));
-                    console.log(ingredientsAndMeasure2);
-                    var ingredientsAndMeasure3 = ((drinkArray.strIngredient3 + " - " + drinkArray.strMeasure3));
-                    console.log(ingredientsAndMeasure3);
-                    var ingredientsAndMeasure4 = ((drinkArray.strIngredient4 + " - " + drinkArray.strMeasure4));
-                    console.log(ingredientsAndMeasure4);
-                    var ingredientsAndMeasure5 = ((drinkArray.strIngredient5 + " - " + drinkArray.strMeasure5));
-                    console.log(ingredientsAndMeasure5);
-                    var ingredientsAndMeasure6 = ((drinkArray.strIngredient6 + " - " + drinkArray.strMeasure6));
-                    console.log(ingredientsAndMeasure6);
-
-
-                    // var ingredientsAndMeasure7 = ((drinkArray.strIngredient7 + " - " + drinkArray.strMeasure7));
-                    // var ingredientsAndMeasure8 = ((drinkArray.strIngredient8 + " - " + drinkArray.strMeasure8));
-                    // var ingredientsAndMeasure9 = ((drinkArray.strIngredient9 + " - " + drinkArray.strMeasure9));
-                    // var ingredientsAndMeasure10 = ((drinkArray.strIngredient10 + " - " + drinkArray.strMeasure10));
-                    // var ingredientsAndMeasure11 = ((drinkArray.strIngredient11 + " - " + drinkArray.strMeasure11));
-                    // var ingredientsAndMeasure12 = ((drinkArray.strIngredient12 + " - " + drinkArray.strMeasure12));
-                    // var ingredientsAndMeasure13 = ((drinkArray.strIngredient13 + " - " + drinkArray.strMeasure13));
-                    // var ingredientsAndMeasure14 = ((drinkArray.strIngredient14 + " - " + drinkArray.strMeasure14));
-                    // var ingredientsAndMeasure15 = ((drinkArray.strIngredient15 + " - " + drinkArray.strMeasure15));
-
 
                     var drinkName = (drinkArray.strDrink + " Details:");
                     console.log(drinkName);
@@ -240,29 +216,40 @@ $().ready(function () {
                     var drinkImageURL = drinkArray.strDrinkThumb;
                     console.log(drinkImageURL);
 
-
                     var h3drinkName = $("<h3>").text(drinkName);
-                    var ingredAndMeasure1 = $("<p>").text(ingredientsAndMeasure1);
-                    var ingredAndMeasure2 = $("<p>").text(ingredientsAndMeasure2);
-                    var ingredAndMeasure3 = $("<p>").text(ingredientsAndMeasure3);
-                    var ingredAndMeasure4 = $("<p>").text(ingredientsAndMeasure4);
-                    var ingredAndMeasure5 = $("<p>").text(ingredientsAndMeasure5);
+                    var ingredientsHeader = $("<h5>").text("Ingredients:")
                     var h5instructionHeader = $("<h5>").text("Instructions:");
                     var pInstructions = $("<p>").text(instructions);
                     var image = $("<img>").attr("src", drinkImageURL);
 
                     searchResultsDiv.append(h3drinkName);
-                    searchResultsDiv.append(ingredAndMeasure1);
-                    searchResultsDiv.append(ingredAndMeasure2);
-                    searchResultsDiv.append(ingredAndMeasure3);
-                    searchResultsDiv.append(ingredAndMeasure4);
-                    searchResultsDiv.append(ingredAndMeasure5);
                     searchResultsDiv.append(h5instructionHeader);
                     searchResultsDiv.append(pInstructions);
                     searchResultsDiv.append(image);
+                    searchResultsDiv.append(ingredientsHeader);
                     $("#results").addClass("scrollbar");
                     $("#results").append(searchResultsDiv);
 
+                    function getIngredientsAndMeasure() {
+                        for (let k = 0; k < ingredArray.length; k++) {
+                            $("<p>").text(ingredArray[k]).appendTo(searchResultsDiv);
+
+                        }
+                    }
+
+                    //Start of Ingredients Rendering
+                    for (var j = 1; j < 16; j++) {
+                        var itemIndex = "strIngredient" + [j];
+                        var ingredientIndex = "strMeasure" + [j];
+                        if (drinkArray[itemIndex] !== null && drinkArray[ingredientIndex] !== null) {
+                            console.log(drinkArray[itemIndex] + " " + drinkArray[ingredientIndex]);
+                            var ingAndMeas = drinkArray[itemIndex] + " - " + drinkArray[ingredientIndex];
+                            ingredArray.push(ingAndMeas);
+                        }
+
+                    };
+
+                    getIngredientsAndMeasure();
 
                 }
 
